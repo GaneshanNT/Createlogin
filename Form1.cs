a9ce4bf4bc01f44a9d173caeba89da1c58260993
Ganeshan N <ganeshancse06@gmail.com>
2018-01-10 13:22:37 +0530

    hello

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Createlogin
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public static string settext = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Register r = new Register();
+            r.Show();
+            this.Hide();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Server=DESKTOP-R7V17QH\\PAAVAISQLEXPRESS; Database=logininfo; Integrated Security=SSPI");
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Connection Open ! ");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+
+            String newcom = "select Username from login where Username = '" + textBox1.Text + "' and Password= '" + textBox2.Text + "'";
+
+            SqlDataAdapter adp = new SqlDataAdapter(newcom, conn);
+
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+            DataTable dt = ds.Tables[0];
+
+            if (dt.Rows.Count >= 1)
+            {
+                settext = textBox1.Text;
+                welcome wc = new welcome();
+                wc.Show();
+                this.Hide(); 
+            }
+
+            else
+            {
+                label3.Text = "Invalid login";
+            }
+
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Register.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; 
+
+namespace Createlogin
+{
+    public partial class Register : MaterialSkin.Controls.MaterialForm
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Server=DESKTOP-R7V17QH\\PAAVAISQLEXPRESS; Database=logininfo; Integrated Security=SSPI");
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Connection Open ! ");
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+            String newcom = "insert into login(Name,Username,Password,Email) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')";
+
+            SqlCommand cmd = new SqlCommand(newcom, conn);
+
+            cmd.ExecuteNonQuery();
+
+            label5.Text = "Registered";
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 r = new Form1();
+            r.Show();
+            this.Hide();
+        }
+    }
+}
